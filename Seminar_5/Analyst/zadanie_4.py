90342bd7eb69788d1dc41f34ae550bc4b8c97ff7
DavidKing911 <avetyan_david@bk.ru>
2022-09-18 20:19:31 +0700

    Задание_4 выполнено

---
+++ Seminar_5/Analyst/zadanie_4.py
@@ -0,0 +1,37 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+data_file = 'AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE'
+file_1 = open("Seminar_5/Analyst/file_1.txt", "w")
+file_2 = open("Seminar_5/Analyst/file_2.txt", "w")
+
+def compression(data): 
+    encoding = '' 
+    prev_char = '' 
+    count = '' 
+    if not data: 
+        return '' 
+    for i in data: 
+        if i != prev_char: 
+            encoding += str(count) + prev_char 
+            count = 1 
+            prev_char = i 
+        else: 
+            count += 1
+    else: 
+        encoding += str(count) + prev_char 
+        return encoding
+
+def recovery(data): 
+    decode = '' 
+    count = '' 
+    for i in data: 
+        if i.isdigit(): 
+            count += i 
+        else: 
+            decode += i * int(count) 
+            count = '' 
+    return decode
+
+file_1.write(recovery(compression(data_file)))
+file_2.write(compression(data_file))
+file_1.close()
+file_2.close()
